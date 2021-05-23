3f21a37f3db0e273939b2cfa47e87e736d50e11c
polmaxfm <67322107+polmaxfm@users.noreply.github.com>
2021-05-23 17:04:11 +0200

    Add files via upload

---
+++ menu-stare.php
@@ -0,0 +1,35 @@
+﻿<br>
+<center><table width="95%" height="60" border="3" cellpadding="3" cellspacing="3" style="border-color:black">
+<td bgcolor="brown">
+<a href="polmax.php" title="Radio Polmax FM kanał główny"><font size="5" color="yellow"><center><b>Polmax FM</b></center></font></a>
+</td>
+
+<td bgcolor="#005887">
+<a href="polmax2.php" title="Polmax 2"><font size="5" color="lime"><center> Polmax 2</center></font></a>
+</td>
+
+<td bgcolor="olive">
+<a href="misiek.php" title="Radio Misiek Muzyka biesiadna"><font size="5" color="yellow"><center> Misiek</center></font></a>
+</td>
+
+<td bgcolor="#97495E">
+<a href="aniolek.php" title="Radio Aniołek - religijne"><font size="5" color="white"><center> Aniołek</center></font></a>     
+</td>
+
+<td bgcolor="#00532A">
+<a href="radio5.php"><font size="5" color="yellow"><center>Radio5</center></font></a>
+</td>
+
+<td bgcolor="#97495E">
+<a href="http://polmax.66ghz.com/wimpy/player.html"><font size="5" color="white"><center>Player</center></font></a>
+</td>
+
+<td bgcolor="green">
+<a href="https://zeno.fm/zabawa/"><font size="5" color="white"><center>Zabawa</center></font></a>
+</td>
+
+<td bgcolor="#005887">
+<a href="http://stream.zeno.fm/favya1hd8yzuv"><font size="5" color="yellow"><center>Rad6</center></font></a>
+</td>
+</table>
+</br>
---
+++ polmax.pls
@@ -0,0 +1,9 @@
+[playlist]
+File1=http://stream.zeno.fm/1gg3kbnzbceuv
+Title1=Radio Polmax FM jakoœæ MP3 192
+Length1=-1
+File2=http://stream.zeno.fm/0mbzxuz0ufeuv
+Title2=Radio Polmax FM jakoœæ aac + MPEG4
+Length2=-1
+NumberOfEntries=2
+Version=2

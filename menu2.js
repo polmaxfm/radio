f69b6538976071ff3ff228d51c126cabd4e71879
polmaxfm <67322107+polmaxfm@users.noreply.github.com>
2021-05-23 14:42:12 +0200

    Update menu2.js

--- menu2.js
+++ menu2.js
@@ -1,3 +1,1 @@
-<script>
-<img src="https://innetapety.pl/images/galeria/big/tapeta-3d-td-31900_18610.jpg" width="800"></img>
-</script>
+document.write('<img src="https://innetapety.pl/images/galeria/big/tapeta-3d-td-31900_18610.jpg" width="800"></img>})();
